17ec93834d1773b8500892c383e91d4532249ed9
ramiboutas <ramiboutas@hotmail.com>
2022-06-06 22:15:06 +0200

    debugging checking no static

--- config/urls.py
+++ config/urls.py
@@ -30,7 +30,7 @@ if settings.DEBUG or True:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
+    # urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
 
