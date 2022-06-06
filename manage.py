a71314407bc0fb2547e377347d658ec062056d40
ramiboutas <ramiboutas@hotmail.com>
2022-06-06 18:32:28 +0200

    prod in manage.py

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     dotenv.read_dotenv()
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
     from django.core.management import execute_from_command_line
 
