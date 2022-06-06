ca3bd27c4f30e33b423a795c48e31ef1e20cbf7c
ramiboutas <ramiboutas@hotmail.com>
2022-06-06 11:15:40 +0200

    prod settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,41 @@
+import os
 from .base import *
 
+
 DEBUG = False
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+ALLOWED_HOSTS = ['ramiboutas.com', 'www.ramiboutas.com' ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+INTERNAL_IPS = ('127.0.0.1')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
+        'TEST': {
+         'NAME': os.environ.get('POSTGRES_TESTS_DB'),
+         },
+    }
+}
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_HSTS_SECONDS = 31536000 # usual: 31536000 (1 year)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_PRELOAD = True
+PREPEND_WWW = True
+
 
 # CACHES = {
 #     'default': {
@@ -8,8 +43,6 @@ DEBUG = False
 #         'LOCATION': '/home/rami/dev/playing-place/wagtail/config/cache',
 #     }
 # }
-
-
 
 try:
     from .local import *
