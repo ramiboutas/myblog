e21d440dc8540bf55d5f20f3cea7aacf55042de9
ramiboutas <ramiboutas@hotmail.com>
2022-06-06 23:54:54 +0200

    debug false in prod settings

--- config/urls.py
+++ config/urls.py
@@ -25,12 +25,12 @@ urlpatterns = urlpatterns + i18n_patterns(
     path('search/', search_views.search, name='search'),
 )
 
-if settings.DEBUG or True:
+if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
-    # urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
 
