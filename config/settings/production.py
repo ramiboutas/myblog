b450e2b6d62fcb75b1bac46914fd143e3afde3c2
ramiboutas <ramiboutas@hotmail.com>
2022-06-08 00:11:36 +0200

    bucket name

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,7 +19,8 @@ INSTALLED_APPS += [
 # Storage
 
 AWS_S3_REGION_NAME = 'fra1'
-AWS_S3_ENDPOINT_URL = f"https://ramiboutas.{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
+AWS_STORAGE_BUCKET_NAME = 'ramiboutas'
+AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
