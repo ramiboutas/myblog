dd115ba1c93b21843dd7abf8904a6a0e6f1b006d
ramiboutas <ramiboutas@hotmail.com>
2022-06-09 07:26:21 +0200

    settings simplified

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@ import os
 from .base import *
 
 
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG') == '1'
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
@@ -24,23 +24,15 @@ if USE_SPACES:
 
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_REGION_NAME = 'fra1'
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_ENDPOINT_URL = f"https://{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
+    AWS_S3_ENDPOINT_URL = f"https://fra1.digitaloceanspaces.com"
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    AWS_S3_SIGNATURE_VERSION = 's3v4'
+    AWS_LOCATION = f"https://fra1.{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
 
-    # static settings
-    AWS_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    STATICFILES_STORAGE = 'config.storage_backends.StaticRootStorage'
+    DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaRootStorage'
 
-    # public media settings
-    AWS_MEDIA_LOCATION = 'media'
-    PUBLIC_MEDIA_LOCATION = 'media'
-    MEDIA_URL = f'https://{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 else:
     STATIC_URL = '/static/'
