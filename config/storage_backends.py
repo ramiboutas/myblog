1e44753559835e79ef9a9ddd8e4a548c0971605c
ramiboutas <ramiboutas@hotmail.com>
2022-06-09 07:56:30 +0200

    import issue fixed

--- config/storage_backends.py
+++ config/storage_backends.py
@@ -1,5 +1,5 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-from settings.conf import settings
+from django.conf import settings
 
 class StaticRootStorage(S3Boto3Storage):
     location = settings.AWS_STATIC_LOCATION
